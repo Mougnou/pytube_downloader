6a2d84adff0737d4edadaa427ea382aece3a528e
Mougnou <46692185+Mougnou@users.noreply.github.com>
2024-02-06 12:14:19 +0100

    Add caption dl
    Actually dl it in main folder dont know why

--- controller.py
+++ controller.py
@@ -9,8 +9,9 @@ class Controller:
     def __init__(self, view):
         self.view = view
 
-        self.view.url_button.configure(command=lambda:self.update_formats())
+        self.view.url_button.configure(command=lambda:self.update_CBB())
         self.view.download_button.configure(command=lambda:self.download())
+        self.view.download_subtitle_button.configure(command=lambda:self.download_subtitle())
         self.view.progress_bar.configure(value=0)
 
     def download(self):
@@ -18,21 +19,46 @@ class Controller:
         format = self.view.format_combobox.get()
         itag = format.split('-')[-1].strip()  # Extract the itag from the format string
         path = self.view.path_entry.get()
+        subtitle = self.view.subtitle_combobox.get().split('-')[-1].strip()
         #check if the video is already downloaded
         
         # Pass the download to a thread
         thread = threading.Thread(target=lambda: self.download_thread(url, itag, path))
         thread.start()
+        if subtitle != "":
+            thread_sub = threading.Thread(target=lambda: self.download_subtitle_thread(url, subtitle, path))
+            thread_sub.start()
+        
+
+    def download_subtitle(self):
+        url = self.view.url_entry.get()
+        subtitle = self.view.subtitle_combobox.get().split('-')[-1].strip()
+        path = self.view.path_entry.get()
+        # Pass the download to a thread
+        thread = threading.Thread(target=lambda: self.download_subtitle_thread(url, subtitle, path))
+        thread.start()
+        
 
     def download_thread(self, url, itag, path):
-        self.model = YouTube(url)
+        
         self.model.register_on_progress_callback(self.on_progress)
         self.model.streams.get_by_itag(itag).download(path)
         tk.messagebox.showinfo("Download", "Download completed")
         self.view.progress_bar.configure(value=0)
 
+    def download_subtitle_thread(self, url, subtitle, path):
+        try:
+            self.model.register_on_progress_callback(self.on_progress)
+            print(f"Downloading subtitle '{subtitle}' for video '{self.model.title}' to '{path}/{self.model.title}.srt'")
+            self.model.captions[subtitle].download(f"{path}/{self.model.title}.srt")
+            tk.messagebox.showinfo("Download", "Download completed")
+            self.view.progress_bar.configure(value=0)
+        except KeyError:
+            print(f"No caption found for language code: {subtitle}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-    def update_formats(self):
+    def update_CBB(self):
         url = self.view.url_entry.get()
         self.model = YouTube(url)
         self.model.register_on_progress_callback(self.on_progress)
@@ -46,6 +72,16 @@ class Controller:
         self.view.format_combobox.configure(values=list) 
         self.view.format_combobox.set(list[0])
         self.view.format_combobox.configure(state="normal")
+
+        list = []
+
+        for caption in self.model.captions:
+            list.append(f"{caption.name} - {caption.code}")
+
+        self.view.subtitle_combobox.configure(values=list) 
+        self.view.subtitle_combobox.set(list[0])
+        self.view.subtitle_combobox.configure(state="normal")
+        
 
     def on_progress(self, stream, chunk, bytes_remaining):
         """Callback function"""
--- view.py
+++ view.py
@@ -24,22 +24,32 @@ class DownloaderApp(ct.CTk):
         self.format_combobox = ct.CTkComboBox(self,state="disabled", values=[""])
         self.format_combobox.grid(row=1, column=1, padx=10, pady=10,columnspan=2, sticky="we")
 
+        self.subtitle_label = ct.CTkLabel(self, text="Subtitle :")
+        self.subtitle_label.grid(row=3, column=0, padx=10, pady=10)
+        self.subtitle_combobox = ct.CTkComboBox(self,state="disabled", values=[""])
+        self.subtitle_combobox.grid(row=3, column=1, padx=10, pady=10,columnspan=2, sticky="we")
+
+
         self.path_label = ct.CTkLabel(self, text="Path :")
-        self.path_label.grid(row=2, column=0, padx=10, pady=10)
+        self.path_label.grid(row=4, column=0, padx=10, pady=10)
         self.path_entry = ct.CTkEntry(self)
-        self.path_entry.grid(row=2, column=1, padx=10, pady=10)
+        self.path_entry.grid(row=4, column=1, padx=10, pady=10)
 
         self.browse_button = ct.CTkButton(self, text="Browse", command=self.open_directory)
-        self.browse_button.grid(row=2, column=2, padx=10, pady=10)
+        self.browse_button.grid(row=4, column=2, padx=10, pady=10)
 
         self.download_button = ct.CTkButton(self, text="Download", state="disabled")
-        self.download_button.grid(row=4, column=0, padx=10, pady=10, columnspan=3, sticky="we")
+        self.download_button.grid(row=5, column=0, padx=10, pady=10, columnspan=3, sticky="we")
+
+        self.download_subtitle_button = ct.CTkButton(self, text="Download Subtitle Only", state="disabled")
+        self.download_subtitle_button.grid(row=6, column=0, padx=10, pady=10, columnspan=3, sticky="we")
 
         self.progress_bar = ttk.Progressbar(self, length=200)
-        self.progress_bar.grid(row=5, column=0,  padx=10, pady=10, sticky="we",columnspan=3)
+        self.progress_bar.grid(row=7, column=0,  padx=10, pady=10, sticky="we",columnspan=3)
 
     def open_directory(self):
         directory = filedialog.askdirectory()
         self.path_entry.delete(0, "end")
         self.path_entry.insert(0, directory)
         self.download_button.configure(state="normal")
+        self.download_subtitle_button.configure(state="normal")
